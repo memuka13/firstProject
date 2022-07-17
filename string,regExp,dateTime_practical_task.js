763f23563b8f4951c956447d39f08a254b5dd338
Mamuka Jincharadze <mamuka_jincharadze@yahoo.com>
2022-07-17 18:56:33 +0400

    Practical Task 6

---
+++ string,regExp,dateTime_practical_task.js
@@ -0,0 +1,23 @@
+
+// 1. Given the string 'ahb acb aeb aeeb adcb axeb'. Write a regular expression that matches the strings ahb, acb, aeb by pattern: letter 'a', any character, letter 'b'
+
+let str = 'ahb acb aeb aeeb adcb axeb'
+
+console.log(str.match(/a.b/g));
+
+
+// 2. Given the string '2 + 3 223 2223'. Write a regex that finds line 2 + 3 without capturing the rest
+
+let str2 = '2 + 3 223 2223'
+
+console.log(str2.match(/[2 + 3]/));
+
+// 3. Get the day, month and year of the current date and output it to the console separately
+
+let now = new Date();
+
+console.log(now);
+console.log(now.getDay());
+console.log(now.getMonth());
+console.log(now.getFullYear());
+
